55358df30ca429d941f655adecdff1f869d1d275
soullaft <soullaft@gmail.com>
2024-02-18 20:15:26 +0400

    added priority, status, workspacetotasks

--- SqlHomeWork/todolist.sql
+++ SqlHomeWork/todolist.sql
@@ -1,6 +1,17 @@
 CREATE SCHEMA IF NOT EXISTS todolist;
 
+CREATE TABLE IF NOT EXISTS todolist.Workspace (
+	id serial primary key,
+	name varchar(250) not null,
+	description varchar not null,
+);
+
 CREATE TABLE IF NOT EXISTS todolist.Statuses (
+	id serial primary key,
+	name varchar(250) not null
+);
+
+CREATE TABLE IF NOT EXISTS todolist.Priority (
 	id serial primary key,
 	name varchar(250) not null
 );
@@ -10,5 +21,11 @@ CREATE TABLE IF NOT EXISTS todolist.Tasks (
 	name varchar(250) not null,
 	description varchar not null,
 	due_date Date not null,
-	status_id integer REFERENCES todolist.Statuses (id)
+	status_id integer REFERENCES todolist.Statuses (id),
+	priority_id integer REFERENCES todolist.Priority (id),
+);
+
+CREATE TABLE IF NOT EXISTS todolist.workspacetotasks (
+	workspace_id REFERENCES todolist.workspace (id),
+	task_id REFERENCES todolist.Tasks (id)
 );
